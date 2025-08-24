b9fa09d373b3b3b3425a531c85648e04afdbf4fa
Ash Bloom <cursusforme12@gmail.com>
2025-08-24 23:46:41 +0100

    Updated files

--- tests/api/test_health.py
+++ tests/api/test_health.py
@@ -8,7 +8,7 @@ API_PASS = os.environ["WAZUH_API_PASS"]
 def test_manager_api_health():
     r = requests.get(f"{API_URL}/security/user/authenticate",
                      auth=(API_USER, API_PASS),
-                     verify=False)
+                     verify="/etc/traefik/certs/cert.pem")
     assert r.status_code == 200
     data = r.json()
     assert "data" in data or "token" in data
