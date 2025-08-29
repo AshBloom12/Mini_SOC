eacecbf4d299e5bda96686a36d3651634ab12b4d
Ash Bloom <cursusforme12@gmail.com>
2025-08-30 00:34:13 +0100

    NEW

--- docker/manager/config/filebeat_module.sh
+++ docker/manager/config/filebeat_module.sh
@@ -2,24 +2,24 @@
 set -e
 
 ## Variables
-FILEBEAT_MODULE_VERSION="4.12.0"
-FILEBEAT_MODULE_DIR="/usr/share/filebeat/module"
-FILEBEAT_VERSION="${FILEBEAT_VERSION:-7.10.2}"
+REPOSITORY="packages-dev.wazuh.com/pre-release"
 
-# Install Wazuh-compatible Filebeat RPM
+# Determine the Wazuh tag for the version
+WAZUH_TAG=$(curl --silent https://api.github.com/repos/wazuh/wazuh/git/refs/tags \
+               | grep '["]ref["]:' \
+               | sed -E 's/.*\"([^\"]+)\".*/\1/' \
+               | cut -c 11- \
+               | grep ^v${WAZUH_VERSION}$)
+
+# Use production repository if tag exists
+if [[ -n "${WAZUH_TAG}" ]]; then
+  REPOSITORY="packages.wazuh.com/4.x"
+fi
+
+# Install the correct Filebeat RPM from Wazuh repo
 curl -L -O https://packages.wazuh.com/4.x/yum/filebeat-${FILEBEAT_VERSION}-1.x86_64.rpm
 yum install -y filebeat-${FILEBEAT_VERSION}-1.x86_64.rpm
 rm -f filebeat-${FILEBEAT_VERSION}-1.x86_64.rpm
 
-# Prepare Filebeat module directory
-mkdir -p ${FILEBEAT_MODULE_DIR}
-
-# Download Wazuh Filebeat module from GitHub using the correct tag
-TMP_DIR=$(mktemp -d)
-git clone --depth 1 --branch "v${FILEBEAT_MODULE_VERSION}" https://github.com/wazuh/wazuh.git "${TMP_DIR}"
-
-# Copy the Wazuh module to Filebeat modules directory
-cp -r "${TMP_DIR}/extensions/filebeat/module/wazuh" "${FILEBEAT_MODULE_DIR}/"
-rm -rf "${TMP_DIR}"
-
-echo "Wazuh Filebeat module for version ${FILEBEAT_MODULE_VERSION} installed successfully."
+# Extract Wazuh Filebeat module tarball
+curl -s https://${REPOSITORY}/filebeat/${WAZUH_FILEBEAT_MODULE} | tar -xvz -C /usr/share/filebeat/module
