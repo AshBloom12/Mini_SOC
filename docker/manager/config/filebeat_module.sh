8ee75636b4758c767e699b6a8c57f12a6eff2d47
Ash Bloom <cursusforme12@gmail.com>
2025-08-30 00:52:10 +0100

    New

--- docker/manager/config/filebeat_module.sh
+++ docker/manager/config/filebeat_module.sh
@@ -4,12 +4,11 @@ set -e
 ## Variables
 WAZUH_VERSION="4.12.0"
 FILEBEAT_MODULE_DIR="/usr/share/filebeat/module"
-FILEBEAT_VERSION="${FILEBEAT_VERSION:-7.10.2}"
 WAZUH_FILEBEAT_MODULE="wazuh-${WAZUH_VERSION}-filebeat-module.tar.gz"
 REPOSITORY="packages.wazuh.com/4.x"
 
-# Install Filebeat RPM from official Wazuh site
-curl -L -O https://packages.wazuh.com/4.x/yum/x86_64/filebeat-7.10.2-1.x86_64.rpm
+# Install Filebeat RPM from Wazuh official repository
+curl -L -O https://packages.wazuh.com/4.x/yum/filebeat-7.10.2-1.x86_64.rpm
 yum install -y filebeat-7.10.2-1.x86_64.rpm
 rm -f filebeat-7.10.2-1.x86_64.rpm
 
