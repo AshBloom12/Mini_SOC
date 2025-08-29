9b544dc2dd16045c92ba3088f2e51af8be116b61
Ash Bloom <cursusforme12@gmail.com>
2025-08-30 00:46:33 +0100

    CI/CD SOC

--- docker/manager/config/filebeat_module.sh
+++ docker/manager/config/filebeat_module.sh
@@ -5,7 +5,6 @@ set -e
 WAZUH_VERSION="4.12.0"
 FILEBEAT_MODULE_DIR="/usr/share/filebeat/module"
 FILEBEAT_VERSION="${FILEBEAT_VERSION:-7.10.2}"
-FILEBEAT_CHANNEL="${FILEBEAT_CHANNEL:-filebeat-oss}"
 WAZUH_FILEBEAT_MODULE="wazuh-${WAZUH_VERSION}-filebeat-module.tar.gz"
 REPOSITORY="packages.wazuh.com/4.x"
 
@@ -14,13 +13,10 @@ curl -L -O https://packages.wazuh.com/4.x/yum/x86_64/filebeat-7.10.2-1.x86_64.rp
 yum install -y filebeat-7.10.2-1.x86_64.rpm
 rm -f filebeat-7.10.2-1.x86_64.rpm
 
-yum install -y filebeat-${FILEBEAT_VERSION}-1.x86_64.rpm
-rm -f filebeat-${FILEBEAT_VERSION}-1.x86_64.rpm
-
 # Prepare Filebeat module directory
 mkdir -p ${FILEBEAT_MODULE_DIR}
 
 # Download Wazuh Filebeat module tarball and extract it
-curl -L https://${REPOSITORY}/filebeat/${WAZUH_FILEBEAT_MODULE} | tar -xvz -C ${FILEBEAT_MODULE_DIR}
+curl -L https://${REPOSITORY}/filebeat/${WAZUH_FILEBEAT_MODULE} | tar -xvzf - -C ${FILEBEAT_MODULE_DIR}
 
 echo "Wazuh Filebeat module for version ${WAZUH_VERSION} installed successfully."
